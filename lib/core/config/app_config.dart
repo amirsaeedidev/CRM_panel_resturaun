8a065e9e40df4f21deca61d7f3108517dab34eb3
amirsaeedidev <amirsaeedi.dev@gmail.com>
2026-08-05 19:11:22 +0330

    add app config

---
+++ lib/core/config/app_config.dart
@@ -0,0 +1,8 @@
+class AppConfig {
+  AppConfig._();
+
+  static const String appName = 'CRM Panel';
+  static const String appVersion = '1.0.0';
+  static const bool enableLogging = true;
+  static const Duration apiTimeout = Duration(seconds: 30);
+}
