5958e77b1ed3ea2ea871be2ea37f274a12ce0898
amirsaeedidev <amirsaeedi.dev@gmail.com>
2026-08-05 19:09:03 +0330

    add AppRoutes

---
+++ lib/core/constants/app_routes.dart
@@ -0,0 +1,14 @@
+class AppRoutes {
+  AppRoutes._();
+
+  static const String splash = '/';
+  static const String login = '/login';
+  static const String dashboard = '/dashboard';
+  static const String orders = '/orders';
+  static const String products = '/products';
+  static const String customers = '/customers';
+  static const String categories = '/categories';
+  static const String reports = '/reports';
+  static const String settings = '/settings';
+  static const String notFound = '/404';
+}
